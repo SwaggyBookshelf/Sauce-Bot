5f7da219afbffd15a789711a26192e1db1201ca1
SwaggyBookshelf <87007414+SwaggyBookshelf@users.noreply.github.com>
2022-09-07 21:11:11 +0200

    Update compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -2,7 +2,7 @@ $PSScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 Set-Location $PSScriptRoot
 pyinstaller --onefile --icon=n.ico --splash=n-1.3.png --window "Sauce Bot 1.0.3.py"
 copy "n.ico" "dist"
-copy "changelog.txt" "dist"
+copy "changelog.md" "dist"
 copy "preferences.config" "dist"
 copy "README.md" "dist"
 cd "dist"
